b668dc9b5cbf684a9685a1b288083e2e9efb14d6
channing <channing@DESKTOP-L38C3M2>
2016-09-10 17:02:42 +0800

    s

--- src/main/resources/com/test/Test.java
+++ src/main/resources/com/test/Test.java
@@ -8,10 +8,10 @@ public class Test {
 		String times[] = time.split(" ");
 		String hm1[] = null;
 		String hm2[] = null;
-		if (times[0].indexOf(":")>=0) {
+		if (times[0].indexOf(":") >= 0) {
 			hm1 = times[0].split(":");
 			hm2 = times[1].split(":");
-		}else if (times[0].indexOf("：")>=0) {
+		} else if (times[0].indexOf("：") >= 0) {
 			hm1 = times[0].split("：");
 			hm2 = times[1].split("：");
 		}
@@ -20,40 +20,39 @@ public class Test {
 		int m1 = Integer.parseInt(hm1[1]);
 		int h2 = Integer.parseInt(hm2[0]);
 		int m2 = Integer.parseInt(hm2[1]);
-		count(h1,m1,h2,m2);
+		count(h1, m1, h2, m2);
 	}
 
-	public static void count(int h1,int m1,int h2,int m2){
+	public static void count(int h1, int m1, int h2, int m2) {
 		int count = 0;
-		if (h1==0) {
-			h1=1;
+		if (h1 == 0) {
+			h1 = 1;
 		}
-		if (h2==24) {
-			h2=23;
+		if (h2 == 24) {
+			h2 = 23;
 		}
-		count = h2-h1-1;
+		count = h2 - h1 - 1;
 		int a1 = 0;
 		int a2 = 0;
-        for(int m=m1; m<60; m++){  
-            double gap = (30 * h1 - 5.5 * m) % 360;  
-            if(-6.0 < gap && gap < 6.0){  
-            	a1++;
-            }  
-        }  
-        for(int m=0; m<m2; m++){  
-            double gap = (30 * h2 - 5.5 * m) % 360;  
-            if(-6.0 < gap && gap < 6.0){  
-            	a2++;
-            }  
-        }  
-        if (a1>0) {
-        	count++;  
-        }
-        if (a2>0) {
-        	count++;  
-        }
+		for (int m = m1; m < 60; m++) {
+			double gap = (30 * h1 - 5.5 * m) % 360;
+			if (-6.0 < gap && gap < 6.0) {
+				a1++;
+			}
+		}
+		for (int m = 0; m < m2; m++) {
+			double gap = (30 * h2 - 5.5 * m) % 360;
+			if (-6.0 < gap && gap < 6.0) {
+				a2++;
+			}
+		}
+		if (a1 > 0) {
+			count++;
+		}
+		if (a2 > 0) {
+			count++;
+		}
 		System.err.println(count);
-}
+	}
 
 }
-
